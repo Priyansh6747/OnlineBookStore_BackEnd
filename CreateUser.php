bb24279c3a3c485d5a463cd388bf094ffdaf6a66
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-09-21 12:43:01 +0530

    Fixed Minor Isuues

--- CreateUser.php
+++ CreateUser.php
@@ -1,6 +1,8 @@
 <?php
     header('Content-Type: application/json');
     header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: Content-Type');
+    
     $connect = new mysqli("localhost","root","","OnlineBookStore");
 
     if($connect->connect_error)
