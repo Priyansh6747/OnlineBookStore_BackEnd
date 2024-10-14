4e690c183613e00fe41d429990e27c95f49cf72e
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-10-14 23:01:04 +0530

    Added Search Functionality Not currently working

---
+++ SearchBook.php
@@ -0,0 +1,18 @@
+﻿<?php
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: Content-Type');
+
+    $connect = new mysqli("localhost","root","","OnlineBookStore");
+        if($connect->connect_error)
+            die("Connection error ". $connect->connect_error);
+
+    $query = isset($_GET['q']) ? $_GET['q'] : '';
+    $sql = "SELECT * FROM book WHERE Name LIKE '%$query%'";
+    $result = $conn->query($sql);
+    $book = [];
+    if($result->num_rows > 0) while ($row = $result->fetch_assoc())
+        $book[] = $row;
+    echo json_encode($book);
+    $connect->close();
+?>
