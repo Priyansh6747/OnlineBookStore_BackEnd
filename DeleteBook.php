258fb20bf7b7046b4e0e1824fc30f3bc2c8a54b5
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-10-30 14:55:51 +0530

    Minor bug fixed

--- DeleteBook.php
+++ DeleteBook.php
@@ -3,13 +3,20 @@
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: Content-Type');
 
-    $connect = new mysqli("localhost","root","","OnlineBookStore");
-        if($connect->connect_error)
-            die("Connection error ". $connect->connect_error);
-
-    $query = isset($_GET['q']) ? $_GET['q'] : '';
-    $sql = "delete from book where B_id = '$query'";
-    $result = $connect->query($sql);
-    echo json_encode(["message" => "Book deleted"]);
+    $connect = new mysqli("localhost", "root", "", "OnlineBookStore");
+    if ($connect->connect_error) 
+        die("Connection error: " . $connect->connect_error);
+    $query = isset($_GET['q']) ? $_GET['q'] : '';    
+    if (!empty($query)) {
+        $stmt = $connect->prepare("DELETE FROM book WHERE B_id = ?");
+        $stmt->bind_param("i", $query);
+        if ($stmt->execute()) {
+            echo json_encode(["message" => "Book deleted"]);
+        } else {
+            echo json_encode(["error" => "Failed to delete book"]);
+        }
+        $stmt->close();
+    } else 
+        echo json_encode(["error" => "No book ID specified"]);
     $connect->close();
 ?>
