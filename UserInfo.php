a60d0109c317069c90b27b4d5f4d1ef9bb328261
sahil1607cms <sahil16072004@gmail.com>
2024-10-31 14:54:14 +0530

    added few comments

--- UserInfo.php
+++ UserInfo.php
@@ -1,16 +1,23 @@
 <?php
+    
     header('Content-Type: application/json');
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: Content-Type');
 
+    //connecting to OnlineBookStore database
     $connect = new mysqli("localhost","root","","OnlineBookStore");
     if($connect->connect_error)
         die("Connection error ". $connect->connect_error);
 
+    //data will be stored in json string format in $data variable
     $data = file_get_contents("php://input");
+    //data will be converted to array format since true is given as parameter 
     $data_decoded = json_decode($data,true);
+    //directly accessing the username like we access the data in other languages stored in array
     $username = $data_decoded['Username'];
+
     $sql_result = $connect->prepare("Select Password from customer where username = ?");
+    //binding the username to the result, s means the username is string 
     $sql_result->bind_param("s",$username);
     if(!$sql_result)
         die("Invalid query " . $connect->error);
