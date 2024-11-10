e8f989250ce596f8d408eee96f902d12394c5a68
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-11-10 14:17:40 +0530

    added checks before accessing the data

--- DeleteFromAddToCart.php
+++ DeleteFromAddToCart.php
@@ -4,25 +4,30 @@
     header('Access-Control-Allow-Headers: Content-Type');
 
     $connect = new mysqli("localhost", "root", "", "OnlineBookStore");
-    if ($connect->connect_error) 
-        die("Connection error: " . $connect->connect_error);
+    if ($connect->connect_error) {
+        die(json_encode(["message" => "Connection error: " . $connect->connect_error]));
+    }
+
     $data = file_get_contents("php://input");
     $data_decoded = json_decode($data, true);
-    if($data_decoded){
-        $BID = $data_decoded["B_id"]?$data_decoded["B_id"]:"";
-        $UID = $data_decoded["UID"]?$data_decoded["UID"]:""; 
-    }
-         
-    if (!empty($BID)) {
+
+    if ($data_decoded && isset($data_decoded["BID"]) && isset($data_decoded["UID"])) {
+        $BID = $data_decoded["BID"];
+        $UID = $data_decoded["UID"];
+
         $stmt = $connect->prepare("DELETE FROM cart WHERE B_id = ? AND UID = ?");
-        $stmt->bind_param("ii", $BID,$UID);
-        if ($stmt->execute()) 
+        $stmt->bind_param("ii", $BID, $UID);
+
+        if ($stmt->execute()) {
             echo json_encode(["message" => "Book deleted"]);
-        else 
+        } else {
             echo json_encode(["message" => "Failed to delete book"]);
-        
+        }
+
         $stmt->close();
-    } else 
-        echo json_encode(["message" => "No book ID specified"]);
+    } else {
+        echo json_encode(["message" => "Invalid data or missing BID/UID"]);
+    }
+
     $connect->close();
 ?>
--- UpdateCart.php
+++ UpdateCart.php
@@ -3,25 +3,36 @@
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: Content-Type');
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     $connect = new mysqli("localhost", "root", "", "OnlineBookStore");
-    if(!$connect) die("Connection failed ". $connect->connect_error);
+    if ($connect->connect_error) {
+        echo json_encode(["message" => "Connection error: " . $connect->connect_error]);
+        exit;
+    }
 
     $data = file_get_contents("php://input");
-    $data_decoded = json_decode($data);
+    $data_decoded = json_decode($data, true);
 
-    if($data_decoded){
+    if ($data_decoded && isset($data_decoded["UID"], $data_decoded["BID"], $data_decoded["Quantity"])) {
         $UID = $data_decoded["UID"];
         $BID = $data_decoded["BID"];
-        $quantity = $data_decoded["quantity"];
+        $quantity = $data_decoded["Quantity"];
+
         $stmt = $connect->prepare("UPDATE `cart` SET `Quantity` = Quantity + ? WHERE `cart`.`UID` = ? AND `cart`.`B_id` = ?");
-        $stmt->bind_param("iii",$quantity,$UID,$BID);
+        $stmt->bind_param("sss", $quantity, $UID, $BID);
 
         if ($stmt->execute()) 
-            echo json_encode(["message" => "cart Updated"]);
-         else 
+            echo json_encode(["message" => "Cart updated"]);
+        else 
             echo json_encode(["message" => "Failed to update cart"]);
 
         $stmt->close();
-    }else
-        echo json_encode(["message" => "Some error has occured"])
+    } else {
+        echo json_encode(["message" => "Invalid or missing UID/BID/Quantity"]);
+    }
+
+    $connect->close();
 ?>
